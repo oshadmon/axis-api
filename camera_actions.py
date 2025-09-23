997e30d7ebe652723b69cb69bfdd3c64fa0ad777
Ori Shadmon <oshadmon@gmail.com>
2025-09-22 17:37:06 -0700

    reorg code
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- camera_actions.py
+++ camera_actions.py
@@ -1,219 +1,102 @@
 import ast
+from importlib.metadata import pass_none
+
 from flask import Flask, jsonify
-
-
 import __support__
+import camera_functions
 
 app = Flask(__name__)
-BASE_URL = "https://166.143.227.89"
+BASE_URL = "166.143.227.89"
 USER = "AnyLog"
 PASSWORD = "OriIsTheBest#1!"
 
-# --- Helper function ---
-def fetch_applications()->list:
+#--- Credentials --
+def configure_client(base_url:str=None, user:str=None, password:str=None):
     """
-    Fetch list of applications from Camera
-    :params:
-        url:str - built URL
-        applications:list - list of applications
-        response:requests.GET - response from GET
-        content:dict - response converted from XML to JSON
-    :return:
-        list of applications
+    Update global config for Python client usage. Flask/browser requests will continue using defaults unless env vars
+    are set.
     """
-    url = f"{BASE_URL}/axis-cgi/applications/list.cgi"
-    applications = []
-
-    response = __support__.get_data(url=url, user=USER, password=PASSWORD)
-    if 200 <= response.status_code < 300:
-        content = __support__.convert_xml(content=response.content.decode())
-        if content and content['reply']['@result'] == 'ok':
-            try:
-                applications = content['reply']['application']
-            except Exception:
-                pass
-
-    return applications
+    global BASE_URL, USER, PASSWORD
+    if base_url:
+        BASE_URL = base_url
+    if user:
+        USER = user
+    if password:
+        PASSWORD = password
 
 
-def fetch_videos()->list:
-    """
-    Fetch list of applications from Camera
-    :params:
-        url:str - built URL
-        applications:list - list of applications
-        response:requests.GET - response from GET
-        content:dict - response converted from XML to JSON
-    :return:
-        list of applications
-    :sample video info:
-    {
-        "@diskid":"SD_DISK",
-        "@eventid":"Motion Recording",
-        "@eventtrigger":"Record video while the rule is active",
-        "@locked":"No",
-        "@recordingid":"20250921_164046_B0AB_B8A44FC5C075",
-        "@recordingstatus":"recording",
-        "@recordingtype":"triggered",
-        "@source":"2",
-        "@starttime":"2025-09-21T20:40:46.627753Z",
-        "@starttimelocal":"2025-09-21T16:40:46.627753-04:00",
-        "@stoptime":"",
-        "@stoptimelocal":"",
-        "video":{
-            "@framerate":"15:1",
-            "@height":"1080",
-            "@mimetype":"video/x-h264",
-            "@resolution":"1920x1080",
-            "@source":"2","@width":"1920"
-        }
-    }
-    """
-    url = f"{BASE_URL}/axis-cgi/record/list.cgi?recordingid=all"
-    videos = {}
+#--- Configurations --
+@app.route("/config/status", methods=["GET"])
+def get_status():
+    output = camera_functions.camera_status(base_url=BASE_URL, user=USER, password=PASSWORD)
+    if output is True:
+        output = {"status": "Running"}
+    else:
+        output = {"status": "Not Running"}
+    return jsonify(output)
 
-    response = __support__.get_data(url=url, user=USER, password=PASSWORD)
-    if 200 <= response.status_code < 300:
-        content = __support__.convert_xml(content=response.content.decode())
-        if content:
-            for user in list(content.keys()):
-                if 'recordings' in content[user] and 'recording' in content[user]['recordings']:
-                    videos[user] = content[user]['recordings']['recording']
+@app.route("/config/info", methods=["GET"])
+def get_configs():
+    output = camera_functions.get_configs(base_url=BASE_URL, user=USER, password=PASSWORD)
+    return jsonify(output)
 
-    return videos
+@app.route("/config/geolocation", methods=["GET"])
+def get_geolocation():
+    output = camera_functions.get_geolocation(base_url=BASE_URL, user=USER, password=PASSWORD)
+    return jsonify(output)
 
-def execute_application(app_name:str, cmd:str):
-    """
-    Execute command against the camera
-    :args:
-        app_name:str - app to execute request against
-        cmd:str - command to execute (start, stop, remove, restart)
-    :params:
-        url:str - generated URL
-    :return:
-        request for POST
-    """
-    url = f"{BASE_URL}/axis-cgi/applications/control.cgi?package={app_name}&action={cmd}"
-    return __support__.post(url=url, user=USER, password=PASSWORD)
+#--- Applications --
+@app.route("/apps/list", methods=["GET"])
+def get_app_list():
+    output = camera_functions.list_applications(base_url=BASE_URL, user=USER, password=PASSWORD)
+    return jsonify(output)
+
+@app.route("/app/<app_name>", methods=["GET"])
+def get_app_info(app_name:str):
+    output = camera_functions.list_applications(base_url=BASE_URL, user=USER, password=PASSWORD, app_name=app_name, status=False)
+    return jsonify(output)
+
+@app.route("/app/<app_name>/status", methods=["GET"])
+def get_app_status(app_name:str):
+    output = camera_functions.list_applications(base_url=BASE_URL, user=USER, password=PASSWORD, app_name=app_name, status=True)
+    return jsonify(output)
+
+@app.route("/app/<app_name>/start", methods=["GET"])
+def start_app(app_name:str):
+    output = camera_functions.execute_application(base_url=BASE_URL, user=USER, password=PASSWORD, app_name=app_name, command='start')
+    return jsonify(output)
+
+@app.route("/app/<app_name>/stop", methods=["GET"])
+def stop_app(app_name:str):
+    output = camera_functions.execute_application(base_url=BASE_URL, user=USER, password=PASSWORD, app_name=app_name, command='stop')
+    return jsonify(output)
+
+@app.route("/app/<app_name>/restart", methods=["GET"])
+def restart_app(app_name:str):
+    output = camera_functions.execute_application(base_url=BASE_URL, user=USER, password=PASSWORD, app_name=app_name, command='restart')
+    return jsonify(output)
 
 
-# --- Routes ---
-@app.route("/list", methods=["GET"])
-@app.route("/list/<app_name>", methods=["GET"])
-def applications_list(app_name=None):
-    """
-    List applications and optionally specific application object(s)
-    :args:
-        app_name:str - specific application to get info for
-    :return:
-        application(s) information
-    """
-    applications = fetch_applications()
-
-    if app_name:
-        for this_app in applications:
-            if this_app['@Name'] == app_name:
-                return jsonify(this_app)
-        return jsonify({"error": "Application not found"}), 404
-
-    return jsonify(applications)
+@app.route("/app/<app_name>/remove", methods=["GET"])
+def remove_app(app_name:str):
+    output = camera_functions.execute_application(base_url=BASE_URL, user=USER, password=PASSWORD, app_name=app_name, command='remove')
+    return jsonify(output)
 
 
-@app.route("/status/<app_name>", methods=["GET"])
-def application_status(app_name:str):
-    """
-    Get status for a specific app
-    :args:
-        app_name:str - application name
-    :params:
-        applications:list - list of applications
-    :return:
-        application status if exists - if not "Unknown"
-    """
-    applications = fetch_applications()
-    for this_app in applications:
-        if this_app["@Name"] == app_name:
-            return jsonify({"status": this_app["@Status"]})
+#--- Recordings --
+@app.route("/recordings/list", methods=["GET"])
+def list_recordings():
+    output = camera_functions.list_recordings(base_url=BASE_URL, user=USER, password=PASSWORD, record_id=None)
+    return jsonify(output)
 
-    return jsonify({"status": "Unknown"}), 404
+@app.route("/recordings/<record_id>", methods=["GET"])
+def recording_info(record_id:str=None):
+    output = camera_functions.list_recordings(base_url=BASE_URL, user=USER, password=PASSWORD, record_id=record_id)
+    return jsonify(output)
 
-@app.route("/execute/<app_name>/start", methods=["GET"])
-def application_start(app_name:str):
-    """
-    Based on app_name start application
-    :return:
-        application status
-    """
-    execute_application(app_name=app_name, cmd='start')
-    return application_status(app_name=app_name)
-
-@app.route("/execute/<app_name>/stop", methods=["GET"])
-def application_stop(app_name:str):
-    """
-    Based on app_name stop application
-    :return:
-        application status
-    """
-    execute_application(app_name=app_name, cmd='stop')
-    return application_status(app_name=app_name)
-
-@app.route("/execute/<app_name>/restart", methods=["GET"])
-def application_restart(app_name:str):
-    """
-    Based on app_name restart application
-    :return:
-        application status
-    """
-    execute_application(app_name=app_name, cmd='restart')
-    return application_status(app_name=app_name)
-
-@app.route("/execute/<app_name>/remove", methods=["GET"])
-def application_remove(app_name:str):
-    """
-    Based on app_name remove application
-    :return:
-        application status
-    """
-    execute_application(app_name=app_name, cmd='start')
-    return application_status(app_name=app_name)
-
-@app.route("/feed/list", methods=["GET"])
-@app.route("/feed/list/<record_id>", methods=["GET"])
-def list_videos(record_id:str=None):
-    response = fetch_videos()
-    if record_id:
-        for user in response:
-            for video in response[user]:
-                if record_id == video['@recordingid']:
-                    return jsonify(video)
-
-    return jsonify(response)
-
-@app.route("/feed/export/<record_id>", methods=["GET"])
-def export_recording(record_id):
-
-    image_info = list_videos(record_id)
-    if not image_info:
-        return jsonify({"Error": f"Failed to locate image with ID {record_id}"})
-    image_info = ast.literal_eval(image_info.data.decode())
-
-    url = f"{BASE_URL}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={record_id}&exportformat=matroska&diskid={image_info['@diskid']}"
-    response = __support__.get_data(url=url, user=USER, password=PASSWORD)
-    if not 200 <= int(response.status_code) < 300:
-        return jsonify({"Error": f"Failed to execute due to network error {response.status_code}"})
-
-    return (
-        response.content,
-        200,
-        {
-            "Content-Type": "video/mp4",
-            "Content-Disposition": "inline"  # let browser decide how to display
-        },
-    )
-
-    # url = f"{BASE_URL}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={record_id}"
-    # response = __support__.get_data(url=url, user=USER, password=PASSWORD)
-    # print(response)
+@app.route("/recordings/export/<record_id>", methods=["GET"])
+def recording_export(record_id:str=None):
+    return camera_functions.export_recording(base_url=BASE_URL, user=USER, password=PASSWORD, record_id=record_id)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
--- camera_functions.py
+++ camera_functions.py
@@ -3,6 +3,26 @@ from __support__ import rest_request, convert_xml
 import ast
 
 #--- Configurations --
+def camera_status(base_url:str, user:str, password:str):
+    status = False
+    url = f"{base_url}/axis-cgi/systemready.cgi"
+    if not base_url.startswith('http'):
+        url = f'https://{url}'
+    response = rest_request(method='POST', url=url,
+                            headers={"Content-Type": "application/json"},
+                            json_payload={
+                                "apiVersion": "1.1", "context": "my context", "method": "systemready",
+                                "params": {"timeout": 20}
+                            },
+                            user=user, password=password)
+
+    try:
+        if response.json()['data']['systemready'].strip().lower() == 'yes':
+            status = True
+    except:
+        pass
+    return status
+
 def get_configs(base_url:str, user:str, password:str):
     """
     Get camera configurations
@@ -15,7 +35,10 @@ def get_configs(base_url:str, user:str, password:str):
     :return:
         dict of information
     """
-    url = f'https://{base_url}/axis-cgi/basicdeviceinfo.cgi'
+    url = f'{base_url}/axis-cgi/basicdeviceinfo.cgi'
+    if not base_url.startswith('http'):
+        url = f'https://{url}'
+
     response = rest_request(method='POST', url=url, headers={"Content-Type": "application/json"},
                             json_payload={"apiVersion": "1.0", "context": "flask-client", "method": "getAllProperties"},
                             user=user, password=password)
@@ -26,7 +49,9 @@ def get_configs(base_url:str, user:str, password:str):
         raise Exception(f"Failed to parse content from {url} (Error: {error})")
 
 def get_geolocation(base_url:str, user:str, password:str):
-    url = f"https://{base_url}/axis-cgi/geolocation/get.cgi"
+    url = f"{base_url}/axis-cgi/geolocation/get.cgi"
+    if not base_url.startswith('http'):
+        url = f'https://{url}'
     response = rest_request(method='GET', url=url, user=user, password=password)
 
     content = convert_xml(content=response.content.decode())
@@ -36,3 +61,83 @@ def get_geolocation(base_url:str, user:str, password:str):
             "long": float(content['PositionResponse']['Success']['GetSuccess']['Location']['Lng'])
         }
     return {}
+
+#--- Applications --
+def __get_applications(base_url:str, user:str, password:str):
+    applications = []
+    url = f"{base_url}/axis-cgi/applications/list.cgi"
+    if not base_url.startswith('http'):
+        url = f'https://{url}'
+    response = rest_request(method='GET', url=url, user=user, password=password)
+    content = __support__.convert_xml(content=response.content.decode())
+    if content and content['reply']['@result'] == 'ok':
+            try:
+                applications = content['reply']['application']
+            except Exception:
+                pass
+
+    return  applications
+
+def list_applications(base_url:str, user:str, password:str, app_name:str=None, status:bool=False):
+    applications = __get_applications(base_url=base_url, user=user, password=password)
+    if applications and app_name:
+        for app in applications:
+            if app['@Name'] == app_name:
+                return {"status": app["@Status"]} if status is True else app
+    return applications
+
+def execute_application(base_url:str, user:str, password:str, app_name:str, command:str):
+    url = f"{base_url}/axis-cgi/applications/control.cgi?package={app_name}&action={command}"
+    if not base_url.startswith('http'):
+        url = f'https://{url}'
+    rest_request(method='POST', url=url, user=user, password=password)
+    return list_applications(base_url=base_url, user=user, password=password, app_name=app_name, status=True)
+
+#--- Recordings ---
+def __get_recordings(base_url:str, user:str, password:str):
+    url = f"{base_url}/axis-cgi/record/list.cgi?recordingid=all"
+    if not url.startswith('http'):
+        url = f"https://{url}"
+    videos = []
+    response = rest_request(method='GET', url=url, user=user, password=password)
+    content = convert_xml(content=response.content.decode())
+    if content:
+        for user in list(content.keys()):
+            if 'recordings' in content[user] and 'recording' in content[user]['recordings']:
+                videos += content[user]['recordings']['recording']
+
+    return videos
+
+
+def list_recordings(base_url:str, user:str, password:str, record_id:str=None):
+    recordings = __get_recordings(base_url=base_url, user=user, password=password)
+    if record_id:
+        for recording in recordings:
+            if recording['@recordingid'] == record_id:
+                return recording
+        return []
+    return recordings
+
+def export_recording(base_url:str, user:str, password:str, record_id:str):
+    recording = list_recordings(base_url=base_url, user=user, password=password, record_id=record_id)
+    if not recording:
+        return {"Error": f"Failed to locate image with ID {record_id}"}
+
+    url = f"{base_url}/axis-cgi/record/export/exportrecording.cgi?schemaversion=1&recordingid={record_id}&exportformat=matroska&diskid={recording['@diskid']}"
+    if not base_url.startswith('http'):
+        url = f"https://{url}"
+
+    response = rest_request(method='GET', url=url, user=user, password=password)
+    if not 200 <= int(response.status_code) < 300:
+        return {"Error": f"Failed to execute due to network error {response.status_code}"}
+
+    return (
+        response.content,
+        200,
+        {
+            "Content-Type": "video/mp4",
+            "Content-Disposition": "inline"  # let browser decide how to display
+        },
+    )
+
+
