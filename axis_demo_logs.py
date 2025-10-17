522170e67e319735c34edcea6aab7c79c2421ff3
Ori Shadmon <oshadmon@gmail.com>
2025-10-17 11:16:22 -0700

    working syslog
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- axis_demo_logs.py
+++ axis_demo_logs.py
@@ -1,5 +1,4 @@
 import argparse
-
 import camera_functions
 import __support__
 import __anylog_support__
@@ -7,15 +6,25 @@ import __anylog_support__
 
 def main():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('anylog_conn', type=str, default='127.0.0.1:32149', help='Operator REST IP:Port')
+    parser.add_argument('anylog_conn', type=str, default='127.0.0.1:32149', help='Operator REST IP:Port')
     parser.add_argument('camera_conn', type=str, default='AnyLog:OriIsTheBest#1!@166.143.227.89', help='access information for Axis camera')
     parser.add_argument('--start-timestamp', type=str, default=None, help='newest')
-    parser.add_argument('--topic', type=str, default='syslog', help='msg client topic for logs')
+    parser.add_argument('--topic', type=str, default='camera-logs', help='msg client topic for logs')
     parser.add_argument('--logical-database', type=str, default='axis', help='')
     args = parser.parse_args()
 
     camera_url, camera_user, camera_password = __support__.extract_credentials(conn_info=args.camera_conn)
     _, serial = __anylog_support__.create_camera_policy(camera_url, camera_user, camera_password)
+
+    is_policy = __support__.check_policy(anylog_conn=args.anylog_conn, where_condition={"id": args.topic})
+    if not is_policy:
+        policy = __anylog_support__.create_syslog_mapping_policy(policy_name=args.topic, dbms="bring [dbms]",
+                                                                 table="bring [table]")
+        __anylog_support__.declare_policy(raw_policy=policy, anylog_conn=args.anylog_conn)
+
+    if not __anylog_support__.check_mqtt(anylog_conn=args.anylog_conn, topic=args.topic):
+        __anylog_support__.declare_mqtt_request(broker='rest', anylog_conn=args.anylog_conn,
+                                                topic=f"topic = (name={args.topic} and policy={args.topic})")
 
     logs = camera_functions.get_syslogs(base_url=camera_url, user=camera_user, password=camera_password)
     for i in range(len(logs)):
@@ -24,7 +33,7 @@ def main():
         logs[i]['serial'] = serial  # camera serial number
 
         logs[i]['timestamp'] = __support__.convert_to_utc(logs[i]['timestamp'])
-
+        logs[i]['message'] = logs[i]['message'].replace("'", '"') # there are messages with single quote in them when they're pushed in
         if not args.start_timestamp:
             logs[i]['timestamp'] = logs[i]['timestamp'].strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
@@ -36,20 +45,9 @@ def main():
                 publish_log.append(log)
     else:
         publish_log = logs
-    data = {}
-    for row in publish_log:
-        for key in row:
-            if key not in data:
-                data[key] = {
-                    'data_type': [],
-                    'count': 0
-                }
 
-            if str(type(row[key])) not in data[key]['data_type']:
-                data[key]['data_type'].append(str(type(row[key])))
-            data[key]['count'] += 1
-    import json
-    print(json.dumps(data, indent=2))
+    # for payload in publish_log:
+    __anylog_support__.publish_data(anylog_conn=args.anylog_conn, payload=publish_log, topic=args.topic)
 
 if __name__ == '__main__':
     main()
