0b0e9a0505761198a08a400ee18a5c720b62596d
Ori Shadmon <oshadmon@gmail.com>
2025-10-16 21:42:06 -0700

    topic param
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- axis_demo.py
+++ axis_demo.py
@@ -78,13 +78,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('anylog_conn', type=str, default='127.0.0.1:32149', help='Operator REST IP:Port')
     parser.add_argument('camera_conn', type=str, default='AnyLog:OriIsTheBest#1!@166.143.227.89', help='access information for Axis camera')
-    parser.add_argument('--policy-id', type=str, default=None, help='Mapping policy ID')
-    parser.add_argument('--local-camera-topic', type=str, default='axis-cameras', help='Topic to use against AnyLog REST message client')
+    parser.add_argument('--topic', type=str, default='axis-data', help='Topic to use against AnyLog REST message client')
     parser.add_argument('--logical-database', type=str, default='axis', help='logical database to store data in')
     args = parser.parse_args()
 
-    if not args.policy_id:
-        args.policy_id = args.local_camera_topic
+
+    if not args.topic:
+        raise ValueError(f"Missing Topic value - topic required")
+
     camera_url, camera_user, camera_password = __support__.extract_credentials(conn_info=args.camera_conn)
 
     # create camera policy if DNE
@@ -94,16 +95,15 @@ def main():
         __anylog_support__.declare_policy(raw_policy=policy, anylog_conn=args.anylog_conn)
 
     # create mapping policy
-    is_policy = __support__.check_policy(anylog_conn=args.anylog_conn, where_condition={"id": args.policy_id})
+    is_policy = __support__.check_policy(anylog_conn=args.anylog_conn, where_condition={"id": args.topic})
     if not is_policy:
-        policy = __anylog_support__.create_mapping_policy(policy_name=args.policy_id, dbms="bring [dbms]", table="bring [table]")
+        policy = __anylog_support__.create_mapping_policy(policy_name=args.topic, dbms="bring [dbms]", table="bring [table]")
         __anylog_support__.declare_policy(raw_policy=policy, anylog_conn=args.anylog_conn)
 
     # msg client for AnyLog
-    if not __anylog_support__.check_mqtt(anylog_conn=args.anylog_conn, topic=args.local_camera_topic):
+    if not __anylog_support__.check_mqtt(anylog_conn=args.anylog_conn, topic=args.topic):
         __anylog_support__.declare_mqtt_request(anylog_conn=args.anylog_conn, broker='rest', port=32149,
-                                            topic=args.local_camera_topic,
-                                            policy_id=args.policy_id)
+                                            topic=f"topic=(name{args.topic} and policy={args.topic})")
 
 
     msg_client = remote_mqtt_client.MqttClient(conn='anyloguser:mqtt4AnyLog!@172.104.228.251:1883',
